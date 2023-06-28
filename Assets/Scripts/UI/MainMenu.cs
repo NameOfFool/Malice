c687f6ec068da4a83ddbd800bf82b3caa1694ff9
NameOfFool <ivan.pauk2008@gmail.com>
2023-06-28 21:13:02 +0300

    i forgot

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,13 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private GameObject menuPanel;
+    private GameObject settingsPanel;
+    public void Start()
+    {
+        menuPanel = GameObject.Find("MenuPanel");
+        settingsPanel = GameObject.Find("SettingsPanel");
+        settingsPanel.SetActive(false);
+    }
     public void OnPlayButton()
     {
         SceneManager.LoadScene(1);
     }
     public void OnSettingsButton()
     {
-
+        SwitchMenu();  
+    }
+    void SwitchMenu()
+    {
+        menuPanel.SetActive(!menuPanel.activeSelf);
+        Debug.Log(settingsPanel);
+        settingsPanel.SetActive(!settingsPanel.activeSelf);   
     }
     public void OnExitButton()
     {
